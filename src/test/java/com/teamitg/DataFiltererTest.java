1553c4558fa7267e0c0652e6601f200af9307b62
Raq Khan <raq.khan@gmail.com>
2024-09-30 11:11:11 +0100

    define basic tests

--- src/test/java/com/teamitg/DataFiltererTest.java
+++ src/test/java/com/teamitg/DataFiltererTest.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 
@@ -14,6 +16,26 @@ class DataFiltererTest {
   void shouldReturnEmptyList_WhenLogFileIsEmpty() throws FileNotFoundException {
     assertTrue(DataFilterer.filterByCountry(openFile("src/test/resources/empty"), "GB").isEmpty());
   }
+
+  @Test @Disabled
+  void shouldReturnEmptyList_WhenLogFileIsInvalid() {  }
+
+  @Test @Disabled
+  void shouldReturnMatchedResults_WhenCountryIsFound() { }
+
+  @Test @Disabled
+  void shouldReturnEmptyList_WhenCountryNotFound() { }
+
+  @Test @Disabled
+  void shouldReturnMatchedResults_WithLowerResponseTime() { }
+
+  @Test @Disabled
+  void shouldReturnMatchedResults_WithHigherResponseTime() { }
+
+  @Test @Disabled
+  void shouldReturnList_WithResponseTimeAboveAverage() { }
+
+
 
   private FileReader openFile(String filename) throws FileNotFoundException {
     return new FileReader(filename);
