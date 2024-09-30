ed27877f6453c0d4b0048b0724beab277eb3825a
Raq Khan <raq.khan@gmail.com>
2024-09-30 11:14:06 +0100

    add RequestLog bean

---
+++ src/main/java/com/teamitg/RequestLog.java
@@ -0,0 +1,40 @@
+package com.teamitg;
+
+import java.time.Instant;
+
+public class RequestLog {
+
+    private Instant requestTimestamp;
+    private String countryCode;
+    private long responseTime;
+
+    public RequestLog(Instant requestTimestamp, String countryCode, long responseTime) {
+        this.requestTimestamp = requestTimestamp;
+        this.countryCode = countryCode;
+        this.responseTime = responseTime;
+    }
+
+    public Instant getRequestTimestamp() {
+        return requestTimestamp;
+    }
+
+    public void setRequestTimestamp(Instant requestTimestamp) {
+        this.requestTimestamp = requestTimestamp;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public long getResponseTime() {
+        return responseTime;
+    }
+
+    public void setResponseTime(long responseTime) {
+        this.responseTime = responseTime;
+    }
+}
